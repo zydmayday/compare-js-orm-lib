14dc851e3262e37a859b61f7284b33d11a75b080
zyd <zydmayday@gmail.com>
2023-03-07 14:17:37 +0900

    change to export

--- knex/migrations/1_create_table.ts
+++ knex/migrations/1_create_table.ts
@@ -1,9 +1,11 @@
-export const up = async (knex) => {
+import { Knex } from "knex";
+
+export const up = async (knex: Knex) => {
   await knex.schema.createTableIfNotExists("some_table", (table) => {
     table.increments();
   });
 };
 
-export const down = async (knex) => {
+export const down = async (knex: Knex) => {
   await knex.schema.dropTableIfExists("some_table");
 };
